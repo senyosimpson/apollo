0cef2d96365109d02c6fe2ac75c38911e989f871
SenYeezus <simpsonsenyo@gmail.com>
2020-04-20 22:53:14 +0200

    first clojure code

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to apollo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/apollo/core.clj
@@ -0,0 +1,30 @@
+(ns apollo.core
+  (:import [javax.sound.midi MidiSystem]))
+
+(defn strToInt
+  "Parses a string into an integer"
+  [val]
+  (Integer/parseInt val)
+)
+
+(defn getSynth
+  "Gets a synth player"
+  [channelNum instrumentNum]
+  (with-open [synth (doto (MidiSystem/getSynthesizer) .open)]
+    (do
+      (let [channel (aget (.getChannels synth) channelNum)
+            instrument (aget (.getAvailableInstruments synth) instrumentNum)]
+        (println (str "Instrument is a " (.getName instrument)))
+        (.loadInstrument synth instrument)
+        (.programChange channel instrumentNum)
+        channel
+      )
+    )
+  )
+)
+
+(defn -main
+  "Get a synthesizer object"
+  [channelNum instrumentNum]
+  (getSynth (strToInt channelNum) (strToInt instrumentNum))
+)
