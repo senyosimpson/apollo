ef0c1ad1d58140bb41fd3205e705a75d81e34931
SenYeezus <simpsonsenyo@gmail.com>
2020-04-29 22:30:50 +0200

    scripts for converting txt to midi
    instrument list

---
+++ scripts/convert_to_midi.py
@@ -0,0 +1,34 @@
+import argparse
+from mido import MidiFile, MidiTrack, Message
+
+
+def populate_track(track, notes, velocities):
+    for note, velocity in zip(notes, velocities):
+        track.append(Message('note_on', note=note, velocity=velocity, time=32))
+        track.append(Message('note_off', note=note, velocity=velocity, time=32))
+    return track
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filename',
+                        type=str,
+                        required=True,
+                        help='The file to convert into midi')
+    args = parser.parse_args()
+    filename = args.filename
+    
+    with open(filename, mode='r') as f:
+        notes = f.read().split()
+        reversed_notes = notes[::-1]
+        notes.extend(reversed_notes)
+        notes = [int(note) for note in notes]
+        velocities = [60 for _ in range(len(notes))]
+    
+    midi = MidiFile(type=1)
+    for i in range(2):
+        track = MidiTrack()
+        track = populate_track(track, notes, velocities)
+        midi.tracks.append(track)
+    
+    midi.save('midifiles/c-major.mid')
