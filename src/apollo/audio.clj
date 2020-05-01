49ca74cb5cd42412d3abf15d2e643870a039e450
SenYeezus <simpsonsenyo@gmail.com>
2020-05-01 08:31:17 +0200

    uses sequencer to play music

--- src/apollo/audio.clj
+++ src/apollo/audio.clj
@@ -3,28 +3,11 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
-
 (def instrument-list
   (str/split (slurp "instruments.txt") #"\n"))
 
-
-(defn get-synth [channelNum instrumentNum]
-  "Gets a synth player"
-  (let [synth (doto (MidiSystem/getSynthesizer) .open)]
-    (let [channel (aget (.getChannels synth) channelNum)
-          instrument (aget (.getAvailableInstruments synth) instrumentNum)]
-        (println (str "Instrument is a " (.getName instrument)))
-        (.loadInstrument synth instrument)
-        (.programChange channel instrumentNum)
-        (fn [volume note duration]
-          (do
-            (.noteOn channel note volume)
-            (Thread/sleep duration)
-          (.noteOff channel note))))))
-
-
 (defn set-instrument-channel [synth instrumentNum channelNum]
-"Sets a channel to the specified instrument"
+  "Sets a channel to the specified instrument"
   (let [channel (aget (.getChannels synth) channelNum)
         instrument (aget (.getAvailableInstruments synth) instrumentNum)]
     (println (str "Setting instrument " (.getName instrument) " on channel " channelNum))
@@ -46,12 +29,9 @@
       (.setReceiver sequencerTransmitter synthesizerReceiver))
     sequencer))
 
-
-(defn play-notes [player notes]
-  (doseq [{:keys [volume note duration]} (map #(hash-map :volume 60 :note % :duration 400) notes)]
-    (player volume note duration)))
-
 (defn play [instrumentNums midifile]
   (let [sequencer (get-sequencer instrumentNums)]
     (set-sequence sequencer midifile)
-    (.start sequencer)))
+    (.start sequencer)
+    (while (true? (.isRunning sequencer)))
+    (.close sequencer)))
--- src/apollo/core.clj
+++ src/apollo/core.clj
@@ -1,6 +1,6 @@
 (ns apollo.core
   (:require [clojure.tools.cli :refer [parse-opts]]
-            [apollo.audio :refer [get-synth play-notes play]]
+            [apollo.audio :refer [play]]
             [apollo.io :refer [read-piano-roll]]))
 
 (defn strToInt [val]
@@ -18,11 +18,6 @@
    :parse-fn strToInt]
   [nil "--roll" "The notes to play"
    :required "Path to piano roll"]])
-  
-; (defn -main [& args]
-;   (let [{:keys [channel instrument roll]} (:options (parse-opts args cli-options))]
-;     (let [notes (read-piano-roll roll)]
-;       (play-notes (get-synth channel instrument) (concat notes (reverse notes))))))
 
 (defn -main []
-  (play [0 60] "midifiles/c-major.mid"))
+  (play [25 60] "midifiles/c-major.mid"))
