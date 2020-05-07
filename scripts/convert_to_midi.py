c20881b65c2923b7e1cded2b49544f632ea75052
SenYeezus <simpsonsenyo@gmail.com>
2020-05-07 22:27:07 +0200

    extra args and small format updates

--- scripts/convert_to_midi.py
+++ scripts/convert_to_midi.py
@@ -15,8 +15,14 @@ if __name__ == "__main__":
                         type=str,
                         required=True,
                         help='The file to convert into midi')
+    parser.add_argument('--num-tracks',
+                        type=int,
+                        required=False,
+                        default=2,
+                        help='The number of tracks to use')
     args = parser.parse_args()
     filename = args.filename
+    num_tracks = args.num_tracks
     
     with open(filename, mode='r') as f:
         notes = f.read().split()
@@ -26,7 +32,8 @@ if __name__ == "__main__":
         velocities = [60 for _ in range(len(notes))]
     
     midi = MidiFile(type=1)
-    for channel in range(2):
+    for i in range(num_tracks):
+        channel = i
         track = MidiTrack()
         track = populate_track(track, channel, notes, velocities)
         midi.tracks.append(track)
--- src/apollo/core.clj
+++ src/apollo/core.clj
@@ -4,20 +4,8 @@
             [apollo.audio :refer [play]]
             [apollo.utils :refer [sanitize]]))
 
-(defn strToInt [val]
-  "Parses a string into an integer"
-  (Integer/parseInt val))
-
 (def cli-options
-  [[nil "--channel" "The channel number"
-    :required "The channel to play on"
-    :default 0
-    :parse-fn strToInt]
-  [nil "--instrument" "The number of the instrument to play"
-   :required "The instrument to play"
-   :default 0
-   :parse-fn strToInt]
-  [nil "--roll" "The notes to play"
+  [[nil "--roll" "The notes to play"
    :required "Path to piano roll"]])
 
 (defn get-instrument-list []
@@ -27,8 +15,8 @@
   (if (empty? instruments)
     mapping
     (recur (into mapping (hash-map (keyword (sanitize (get instruments 0))) iteration))
-            (inc iteration)
-            (vec (rest instruments))))))
+           (inc iteration)
+           (vec (rest instruments))))))
 
 (defn get-instrument-numbers [instruments instrument-names]
   (vec (map #((keyword %) instruments) instrument-names)))
@@ -36,5 +24,5 @@
 (defn -main []
   (let [instruments (get-instrument-list)]
     (play
-      (get-instrument-numbers instruments ["violin" "violin"])
+      (get-instrument-numbers instruments ["violin"])
       "midifiles/c-major.mid")))
