ad35f48e526d878f272fafeeb1fe519506a8e778
SenYeezus <simpsonsenyo@gmail.com>
2020-05-09 17:39:27 +0200

    add cider-nrepl plugin

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "1.0.194"]]
+  :plugins [[cider/cider-nrepl "0.24.0"]]
   :main ^:skip-aot apollo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
