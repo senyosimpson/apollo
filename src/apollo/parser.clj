c1a54c82f14d905e72d0343ef2fa49fbb4a1e37c
SenYeezus <simpsonsenyo@gmail.com>
2020-05-23 22:42:45 +0200

    parser builds score representations

--- src/apollo/parser.clj
+++ src/apollo/parser.clj
@@ -1,28 +1,141 @@
 (ns apollo.parser
   (:require [clojure.string :as str]
-            [apollo.midi :refer [get-octave-base-note
-                                 note-to-midi
-                                 note-offsets]]))
+            [apollo.utils :refer [sanitize]]))
 
-(def note-offsets-map (note-offsets ["c" "c#" "d" "d#" "e" "f"
-                                     "f#" "g" "g#" "a" "a#" "b"]))
 
-(defn split-meta-score [apl]
-  "Splits an apl file into metadata and the musical score"
-  (str/split (slurp apl) #"\n"))
+(defn get-instrument-list []
+  (loop [mapping {}
+         iteration 0
+         instruments (str/split (slurp "instruments.txt") #"\n")]
+  (if (empty? instruments)
+    mapping
+    (recur (into mapping (hash-map (sanitize (first instruments)) iteration))
+           (inc iteration)
+           (rest instruments)))))
+
+(def instrument-names (get-instrument-list))
+
+
+(defn get-instrument-number [instrument]
+  (get instrument-names (sanitize instrument)))
+
 
 (defn get-instrument-name [meta]
   (subs meta 0 (str/index-of meta ":")))
 
-(defn get-score-octave [meta]
-  "Get the octave of the musical score"
-  (let [octave (subs meta (str/index-of meta ":"))]
-    (if (= ":" octave)
-      (get-octave-base-note)
-      (get-octave-base-note (Integer/parseInt (str (last octave)))))))
 
-(defn convert-score-to-midi [meta score]
-  "Convert a musical score into the relevant midi notes"
-  (map (partial note-to-midi (get-score-octave meta) note-offsets-map)
-       (str/split score #" ")))
-         
+(defn note-offsets [notes]
+  "Gets the offsets of a note from a base note. i.e D has an offset of 2 relative to C.
+  Returns a mapping from note to offset e.g {C 0, C# 1, D 2, D# 3}"
+  (loop [mapping {}
+         iteration 0
+         notes notes]
+    (if (empty? notes)
+      mapping
+      (recur (into mapping (hash-map (first notes) iteration))
+             (inc iteration)
+             (rest notes)))))
+
+
+(def note-offsets-map (note-offsets ["c" "c#" "d" "d#" "e" "f"
+                                     "f#" "g" "g#" "a" "a#" "b"]))
+
+
+(defn split-score-notes [apl]
+  "Splits an apl file into score and the musical score"
+  (str/split (slurp apl) #"\n"))
+
+
+(defn get-score-info [score]
+  "Get the high level score information from the score"
+  (first (str/split score #"\n")))
+
+
+(defn get-notes [score]
+  "Get the notes from the score"
+  (last (str/split score #"\n")))
+
+
+(defn get-octave-base-note
+  "Gets the first midi note number for an octave"
+  ([octave]
+    (+ 24 (* 12 (- octave 1))))
+  ([]
+    (get-octave-base-note 4)))
+
+
+(defn get-score-octave [score]
+  "Get the octave of the musical score"
+  (let [octave (subs score (str/index-of score ":"))]
+    (if (= ":" octave)
+        4
+        (Integer/parseInt (str (last octave))))))
+
+
+; (defn convert-score-to-midi [meta score]
+;   "Convert a musical score into the relevant midi notes"
+;   (map (partial note-to-midi (get-score-octave meta) note-offsets-map)
+;        (str/split score #" ")))
+
+
+(defn to-midi [note octave]
+  "
+  Converts a string representation of a note to a midi number.
+  For example, middle c -> 60
+
+  Arguments:
+    octave - the octaves the note is in (e.g 4)
+    note - the note in string representation to convert
+  "
+
+  (+ (get-octave-base-note octave) (get note-offsets-map note)))
+
+
+(defn get-note-data [notes octave]
+  "
+  Creates a vector of notes with their corresponding data. This is the core structure
+  that defines the data needed for a note to be played by the sound engine. It is a 
+  hash map with the data:
+    {
+      :note \"c\"
+      :midi-note 60
+      :volume 60
+      :channel 0
+      :duration 300
+    }
+
+  Arguments:
+    notes - a list of notes
+    octave - the octave the notes are in (i.e 4)
+  "
+  (loop [note-data []
+         notes notes]
+    (if (empty? notes)
+      note-data
+      (recur (conj note-data {:note (first notes)
+                              :midi-note (to-midi (first notes) octave)
+                              :volume 60
+                              :channel 0
+                              :duration 2})
+             (rest notes)))))
+
+
+(defn build-score-repr [score]
+  "
+  Builds the internal representation of a score. This is the main structure used
+  by the audio engine. This is a hash map with the data:
+    {
+      :instrument acoustic grand piano
+      :instrument-no 0
+      :octave 4
+      :notes (note-repr)
+    }
+  "
+  (let [score-info (get-score-info score)
+        instrument (get-instrument-name score-info)
+        octave (get-score-octave score-info)
+        notes (str/split (get-notes score) #" ")]
+    {:instrument instrument
+     :instrument-number (get-instrument-number instrument)
+     :octave octave
+     :notes (get-note-data notes octave)}))
