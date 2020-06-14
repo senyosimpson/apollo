76211ae75078df97b3af9b3b14239c669132ac00
SenYeezus <simpsonsenyo@gmail.com>
2020-06-14 16:03:16 +0200

    feat: additional tests

--- test/apollo/parser_test.clj
+++ test/apollo/parser_test.clj
@@ -1,6 +1,7 @@
 (ns apollo.parser-test
   (:require [clojure.test :refer :all]
-            [apollo.parser :refer :all]))
+            [apollo.parser :refer :all]
+            [apollo.constants-test :refer :all]))
 
 
 (deftest get-instrument-number-test
@@ -29,7 +30,7 @@
 ; should use a fixture here
 (deftest get-score-notes-test
   (let [score (slurp "test/apollo/testscore.apl")]
-    (is (= (get-score-notes score) "c d e f g a b c c b a g f e d c"))))
+    (is (= (get-score-notes score) "c4 d e f2 g a b c c b a g f e d c"))))
 
 
 (deftest get-octave-base-note-test
@@ -38,26 +39,32 @@
 
 
 (deftest get-score-octave-test
-  (let [score-info (get-score-metadata (slurp "test/apollo/testscore.apl"))]
-    (is (= (get-score-octave score-info) 4))))
+  (let [score-metadata (get-score-metadata (slurp "test/apollo/testscore.apl"))]
+    (is (= (get-score-octave score-metadata) 4))))
 
 
 (deftest to-midi-note-test
-  (is (= (to-midi-note "c" 4) 60))
-  (is (= (to-midi-note "d" 4) 62)))
+  (is (= (to-midi-note "c" 4) '(60)))
+  (is (= (to-midi-note "d" 4) '(62))))
 
 
 (deftest get-note-duration-test
   (is (= (get-note-duration "c" true) 1))
   (is (= (get-note-duration "c4") 4))
+  (is (= (get-note-duration "ceg2") 2))
   (is (nil? (get-note-duration "c"))))
 
 
 (deftest get-note-letter-test
-  (is (= (get-note-letter "c") "c")
-  (is (= (get-note-letter "c4") "c"))))
+  (is (= (get-note-letter "c") "c"))
+  (is (= (get-note-letter "c4") "c"))
+  (is (= (get-note-letter "ceg4") "ceg")))
 
 
 (deftest get-valid-duration-test
   (is (= (get-valid-duration 4 nil) 4))
   (is (= (get-valid-duration 4 2) 2)))
+
+
+(deftest apl-score-from-file-test
+  (is (= (apl-score-from-file "test/apollo/testscore.apl") apl-testscore)))
--- test/apollo/testscore.apl
+++ test/apollo/testscore.apl
@@ -1,2 +1,2 @@
 Acoustic Grand Piano: o4
-c d e f g a b c c b a g f e d c
+c4 d e f2 g a b c c b a g f e d c
